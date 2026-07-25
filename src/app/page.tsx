dfe9d354457a263cc5a23cdc9a95d3c1320eb345
Yusuf Safa <127033214+yusufsaafa@users.noreply.github.com>
2026-07-26 00:37:50 +0300

    feat: place case study after industries

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import { CinematicHero } from "./components/CinematicHero";
 import { IndustriesSection } from "./components/IndustriesSection";
+import { CaseStudySection } from "./components/CaseStudySection";
 
 export default function Home() {
   return (
     <>
       <CinematicHero />
       <IndustriesSection />
+      <CaseStudySection />
     </>
   );
 }
