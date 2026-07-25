b52543709c7cf3e03491d923bcf73569b04171f2
Yusuf Safa <127033214+yusufsaafa@users.noreply.github.com>
2026-07-26 00:46:29 +0300

    feat: wire navigation anchors across homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,26 @@ import { CinematicHero } from "./components/CinematicHero";
 import { IndustriesSection } from "./components/IndustriesSection";
 import { CaseStudySection } from "./components/CaseStudySection";
 import { TechnicalRequestSection } from "./components/TechnicalRequestSection";
+import { GlobalNavigation } from "./components/GlobalNavigation";
+
+const anchorStyle = { scrollMarginTop: "92px" };
 
 export default function Home() {
   return (
     <>
-      <CinematicHero />
-      <IndustriesSection />
-      <CaseStudySection />
-      <TechnicalRequestSection />
+      <GlobalNavigation />
+      <div id="experience" style={anchorStyle}>
+        <CinematicHero />
+      </div>
+      <div id="industries" style={anchorStyle}>
+        <IndustriesSection />
+      </div>
+      <div id="case-study" style={anchorStyle}>
+        <CaseStudySection />
+      </div>
+      <div id="technical-brief" style={anchorStyle}>
+        <TechnicalRequestSection />
+      </div>
     </>
   );
 }
