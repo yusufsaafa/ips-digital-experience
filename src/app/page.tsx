7d9fdd1b9db2b33693b973c04f2b22bcce28d631
Yusuf Safa <127033214+yusufsaafa@users.noreply.github.com>
2026-07-27 14:39:08 +0300

    refactor(home): replace cinematic flow with clear product-led page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,5 @@
-import { CinematicHero3D } from "./components/CinematicHero3D";
-import { IndustriesSection } from "./components/IndustriesSection";
-import { CaseStudySection } from "./components/CaseStudySection";
-import { EngineeringProcessSection } from "./components/EngineeringProcessSection";
-import { TechnicalRequestSection } from "./components/TechnicalRequestSection";
-import { GlobalNavigation } from "./components/GlobalNavigation";
-import { SectionReveal } from "./components/SectionReveal";
+import { ModernHome } from "./components/ModernHome";
 
 export default function Home() {
-  return (
-    <>
-      <GlobalNavigation />
-      <div id="experience" style={{ scrollMarginTop: "92px" }}>
-        <CinematicHero3D />
-      </div>
-      <SectionReveal id="industries" index="02" label="Application environments">
-        <IndustriesSection />
-      </SectionReveal>
-      <SectionReveal id="case-study" index="03" label="Failure to control" delay={80}>
-        <CaseStudySection />
-      </SectionReveal>
-      <SectionReveal id="engineering-process" index="04" label="Evidence to validation" delay={90}>
-        <EngineeringProcessSection />
-      </SectionReveal>
-      <SectionReveal id="technical-brief" index="05" label="Start an engineering brief" delay={100}>
-        <TechnicalRequestSection />
-      </SectionReveal>
-    </>
-  );
+  return <ModernHome />;
 }
